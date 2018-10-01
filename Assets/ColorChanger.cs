906d615a1661e394686a1327749514639c3e2bab
Felix Zander <felix.zander@elev.ga.lbs.se>
2018-10-01 10:40:35 +0200

    gjorde att man kan snurra på bollen

---
+++ Assets/ColorChanger.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChanger : MonoBehaviour {
+
+    public SpriteRenderer rend;
+    public Color color;
+
+	// Use this for initialization
+	void Start ()
+    {
+        rend.color = color;
+        rend.color = new Color(1, 0.25f, 0, 33f);
+
+        transform.position = new Vector3(-5, transform.position.y);
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(0, 0, 360 * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.RightArrow)) { 
+        transform.Rotate(0, 0, -360 * Time.deltaTime);
+    }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(2f * Time.deltaTime, 0, 0, Space);
+        }
+
+        transform.Translate(0, 0, 0);
+
+
+
+
+    }
+        
+}
